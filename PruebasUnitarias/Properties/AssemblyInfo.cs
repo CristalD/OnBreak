68693bb8d650355b9598cf2f041fb3fdb4b080de
CristalD <54589482+CristalD@users.noreply.github.com>
2026-06-21 21:45:10 -0400

    Add files via upload

---
+++ PruebasUnitarias/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebasUnitarias")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PruebasUnitarias")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("409aa10a-f5f4-4c17-a3f2-4a1d6fc30479")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PruebasUnitarias/ValorizadorTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnBreak.Negocio;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class ValorizadorTest
+    {
+        [TestMethod()]
+        public void AsistentesNegativoTest()
+        {
+            //Arrange
+            OnBreak.Negocio.Valorizador val = new Valorizador();
+
+            //ACT
+            try
+            {
+                val.CalcularValorEvento(2, -5, 2.5, 5);
+            }
+            catch (Exception ex)
+            {
+                //Assert
+                StringAssert.Contains(ex.Message, "Asistentes debe ser un numero positivo y mayor a 0");
+                return;
+            }
+            Assert.Fail("No se generó excepción por Asistentes negativo");
+        }
+
+        [TestMethod]
+        public void AsistentesCeroTest()
+        {
+            //Arrange
+            OnBreak.Negocio.Valorizador val = new Valorizador();
+            //ACT
+            try
+            {
+                val.CalcularValorEvento(2, 0, 2.5, 5);
+            }
+            catch (Exception ex)
+            {
+                //Assert
+                StringAssert.Contains(ex.Message, "asistentes no puede ser 0");
+                return;
+            }
+            Assert.Fail("No se generó excepción por Asistentes CERO");
+        }
+
+        [TestMethod]
+        public void AdicionalMayorDiezTest()
+        {
+            //Arrange
+            OnBreak.Negocio.Valorizador val = new Valorizador();
+            //ACT
+            try
+            {
+                val.CalcularValorEvento(11, 5, 2.5, 5);
+            }
+            catch (Exception ex)
+            {
+                //Assert
+                StringAssert.Contains(ex.Message, "Maxima para personal adicional es 10");
+                return;
+            }
+            Assert.Fail("No se generó excepción por Personal Adicional mayor a 10");
+        }
+    }
+}
